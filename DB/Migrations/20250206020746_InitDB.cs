5728152095fc0bcbc7eaf590d02bebf8e9b66363
Duvan Yesid <yesidrodriguez305@gmail.com>
2025-02-05 21:41:35 -0500

    Add initial database schema migration

---
+++ DB/Migrations/20250206020746_InitDB.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DB.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitDB : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Brand",
+                columns: table => new
+                {
+                    BrandId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Brand", x => x.BrandId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Beer",
+                columns: table => new
+                {
+                    BeerId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    BrandId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Beer", x => x.BeerId);
+                    table.ForeignKey(
+                        name: "FK_Beer_Brand_BrandId",
+                        column: x => x.BrandId,
+                        principalTable: "Brand",
+                        principalColumn: "BrandId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Beer_BrandId",
+                table: "Beer",
+                column: "BrandId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Beer");
+
+            migrationBuilder.DropTable(
+                name: "Brand");
+        }
+    }
+}
