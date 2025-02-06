d71bfa94797a3d11d56e0eb3fa150e9d9954a2fb
Duvan Yesid <yesidrodriguez305@gmail.com>
2025-02-05 21:42:52 -0500

    Add BeerController to handle GET requests for beers

---
+++ CodeFirstApi/Controllers/BeerController.cs
@@ -0,0 +1,24 @@
+﻿using DB;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodeFirstApi.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class BeerController : ControllerBase
+  {
+    private readonly BarContext _context;
+
+    public BeerController(BarContext context)
+    {
+      _context = context;
+    }
+
+    [HttpGet]
+    public IActionResult Get()
+    {
+      return Ok(_context.Beers);
+    }
+  }
+}
